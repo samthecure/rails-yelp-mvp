74027b73b3ec74091b884079fc768b510fe0e735
Samuel Magalhaes <samuel.f.magalhaes@gmail.com>
2024-01-25 20:01:08 +0100

    phone_number nature changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_25_170034) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_25_185607) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
