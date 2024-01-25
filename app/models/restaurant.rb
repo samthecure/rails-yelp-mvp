7e9b1759159d35c9fdd86a7f889351632712f26c
Samuel Magalhaes <samuel.f.magalhaes@gmail.com>
2024-01-25 19:45:55 +0100

    validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ApplicationRecord
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian] }
+
   has_many :reviews, dependent: :destroy
 end
