0e921c6106891379601e0bb7d59409e8c063a13f
Samuel Magalhaes <samuel.f.magalhaes@gmail.com>
2024-01-26 18:58:17 +0100

    seed validé avec 5 entrées

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,40 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Supprime toutes les données existantes de la table Restaurants
+Restaurant.destroy_all
+
+# Crée 5 restaurants avec des informations personnalisées
+Restaurant.create!(
+  name: "Chez Antoine",
+  address: "123 rue de la bonne bouffe",
+  phone_number: "01-22-33-44-55",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Sakura Sushi",
+  address: "12 rue du japon",
+  phone_number: "555-5678",
+  category: "japanese"
+)
+
+Restaurant.create!(
+  name: "Chez Andrea",
+  address:"12 rue de la pizza",
+  phone_number: "01-12-13-14-15",
+  category:"italian"
+)
+
+Restaurant.create!(
+  name: "La bonne frite",
+  address:"21 rue des statues",
+  phone_number: "01-32-98-14-81",
+  category:"belgian"
+)
+
+Restaurant.create!(
+  name: "Le petit Pekinois",
+  address:"176 rue du camarade Mao",
+  phone_number: "01-02-10-64-00",
+  category:"chinese"
+)
+
+puts 'Seed completed successfully!'
